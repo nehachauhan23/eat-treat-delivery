a569e47881f7887388c7d3b53de59abd4e2435ea
Neha Chauhan <cneha15@gmail.com>
2026-06-04 16:12:02 +0530

    frontend remove extra /

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -3,5 +3,5 @@ const API_URL = import.meta.env.DEV
   ? import.meta.env.VITE_API_URL_DEV
   : import.meta.env.VITE_API_URL_PROD;
 export const api = axios.create({
-    baseURL: API_URL+'/api'
+    baseURL: API_URL+'api'
 })
