cbe2c6461730e81aba9ac6980c7936c3f5eded6a
Neha Chauhan <cneha15@gmail.com>
2026-06-04 15:28:18 +0530

    logs

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,10 @@
 import { io } from "socket.io-client";
+
+console.log({
+  DEV: import.meta.env.DEV,
+  PROD: import.meta.env.PROD,
+  MODE: import.meta.env.MODE,
+});
 const SOCKET_URL = import.meta.env.DEV
   ? import.meta.env.VITE_SOCKET_URL_DEV
   : import.meta.env.VITE_SOCKET_URL_PROD;
